8f060b6926bfb1d98f3145de74bf8b09bcc2f471
AnaCarolinaNeves <aninha_0512_@hotmail.com>
2023-03-21 16:05:40 -0300

    atualizacao typeorm

---
+++ typeORM/src/routes/user.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import {UserController} from "../controllers";
+
+const routes = Router();
+
+routes.post('/', UserController.create);
+routes.put('/', UserController.update);
+
+export default routes;
