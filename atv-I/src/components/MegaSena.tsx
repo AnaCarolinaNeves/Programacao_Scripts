eeb29d7b6dea4cfd538ccd853e339ff5d0df0085
Ana Carolina <aninha_0512_@hotmail.com>
2023-04-16 16:06:35 -0300

    add atv-i

---
+++ atv-I/src/components/MegaSena.tsx
@@ -0,0 +1,47 @@
+import '../styles/megaSena.css';
+import trevoMega from "../assets/trevo-megasena.png";
+import { UseContexto } from '../hooks';
+
+function Megasena() {
+
+    const {megasena} = UseContexto();
+
+    return (
+        <div className="container">
+            <div className='row'>
+                <div className='column'>
+                    <div className='image-trevo'>
+                        <img
+                            src={trevoMega}
+                            alt='trevo-megasena'
+                        />
+                        <div className='text-megasena'>MEGA-SENA</div>
+                    </div>
+
+                    <p style={{marginLeft: '45px'}}>
+                        Estimativa de prêmio do próximo <br/> concurso. Sorteio em {megasena.dataProximoConcurso}:
+                    </p>
+
+                    <div className='text-premio'>
+                        R${megasena.valorEstimadoProximoConcurso}
+                    </div>
+
+                </div>
+                <div className='column'>
+                    {megasena.dezenas.map(item => (
+                        <div className='circle'>{item.split('')}</div>
+                    ))}
+                    
+                    <div className='text-acc'>ACUMULOU!</div>
+
+                    <p>
+                        Concurso {megasena.numeroDoConcurso} {megasena.dataPorExtenso}
+                    </p>
+                </div>
+            </div>
+            <hr />
+        </div>
+    )
+}
+
+export default Megasena;
