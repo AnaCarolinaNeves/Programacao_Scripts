da06285972eef8ac6308c4bb4df07cc9b384e17f
Ana Carolina <aninha_0512_@hotmail.com>
2023-04-16 16:11:01 -0300

    update

--- typeORM/src/routes/spent.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import {SpentController} from "../controllers";
-
-const routes = Router();
-
-routes.post('/', SpentController.create); 
-
-export default routes;
--- typeORM/src/routes/user.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import {UserController} from "../controllers";
-
-const routes = Router();
-
-routes.post('/', UserController.create);
-routes.put('/', UserController.update);
-
-export default routes;
